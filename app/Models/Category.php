434dbaef9ca3ac3c60d2e1f0991dc92e50775331
Shailly Dixit <shailly@192.168.1.6>
2024-04-18 02:57:07 +0530

    category module created

---
+++ app/Models/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Category extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+
+    public static function check_category($data,$category_id)
+	{
+		 $companies = DB::table('categories') 
+					->where('status', '=', '1')
+					->where('category_name', '=', $data['category_name'])
+					->where('id', '!=', $category_id)
+				 	->first();
+		return $companies;		
+	}
+}
--- resources/views/elements/bottom_js.blade.php
+++ resources/views/elements/bottom_js.blade.php
@@ -8,6 +8,8 @@
 	<script src="{{asset('backend/assets/js/jquery.min.js')}}"></script>
 	<script src="{{asset('backend/assets/js/jquery.validate.min.js')}}"></script>
 
+	<script src="{{asset('backend/assets/js/pace.min.js')}}"></script>
+
 <!--plugins-->
 	<script src="{{asset('backend/assets/plugins/simplebar/js/simplebar.min.js')}}"></script>
 	<script src="{{asset('backend/assets/plugins/metismenu/js/metisMenu.min.js')}}"></script>
@@ -26,6 +28,28 @@
 		new PerfectScrollbar(".app-container")
 	</script>
 	<!--Password show & hide js -->
+	<script type="text/javascript" src="{{asset('backend/assets/js/sweetalert2.all.min.js')}}"></script>
+	<!-- <script src="{{asset('backend/assets/js/vendors.min.js')}}"></script> -->
+
+	<script src="{{asset('backend/assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+	<script src="{{asset('backend/assets/plugins/datatable/js/dataTables.bootstrap5.min.js')}}"></script>
+	<script>
+		$(document).ready(function() {
+			$('#example').DataTable();
+		  } );
+	</script>
+	<script>
+		$(document).ready(function() {
+			var table = $('#example2').DataTable( {
+				lengthChange: false,
+				buttons: [ 'copy', 'excel', 'pdf', 'print']
+			} );
+		 
+			table.buttons().container()
+				.appendTo( '#example2_wrapper .col-md-6:eq(0)' );
+		} );
+	</script>
+
 	<script>
 		$(document).ready(function () {
 			$("#show_hide_password a").on('click', function (event) {
